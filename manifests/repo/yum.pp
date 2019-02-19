c042b265dda77de0265135c84f03ef88891a86b1
Quien Sabe <qs5779@mail.com.com>
2019-02-19 07:43:27 -0500

    only add epel repo if not defined elsewhere

--- manifests/repo/yum.pp
+++ manifests/repo/yum.pp
@@ -3,14 +3,15 @@ class ansible::repo::yum (
   Integer $epel_version
 ) {
 
-  yumrepo { 'epel':
-    ensure         => 'present',
-    descr          => "Extra Packages for Enterprise Linux ${epel_version} - \$basearch",
-    enabled        => '1',
-    failovermethod => 'priority',
-    gpgcheck       => '1',
-    gpgkey         => "https://archive.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-${epel_version}",
-    mirrorlist     => "https://mirrors.fedoraproject.org/metalink?repo=epel-${epel_version}&arch=\$basearch",
+  if ! defined(Yunrepo['epel']) {
+    yumrepo { 'epel':
+      ensure         => 'present',
+      descr          => "Extra Packages for Enterprise Linux ${epel_version} - \$basearch",
+      enabled        => '1',
+      failovermethod => 'priority',
+      gpgcheck       => '1',
+      gpgkey         => "https://archive.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-${epel_version}",
+      mirrorlist     => "https://mirrors.fedoraproject.org/metalink?repo=epel-${epel_version}&arch=\$basearch",
+    }
   }
-
 }
