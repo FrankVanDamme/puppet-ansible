83a70c6eff40b284d98b0fca5744ae5bb452abf8
Frank Van Damme <frank.van.damme@vub.be>
2021-10-28 15:40:29 +0200

    user password was ineffective

--- manifests/user.pp
+++ manifests/user.pp
@@ -10,14 +10,13 @@ class ansible::user (
         managehome => true,
         shell      => '/bin/bash',
         home       => '/home/ansible',
-        password   => $ansible::user::password
     }
-    
+
     $sudo_user = $ansible::sudo_user ? {
         undef   => "ALL",
         default => "${ansible::sudo_user}",
     }
-    
+
     # if Ansible configuration is set to use sudo, we need a sudo rule to allow access
     if ( $ansible::become_method == 'sudo' or $ansible::become_method == undef ) {
         sudo::conf { 'ansible':
