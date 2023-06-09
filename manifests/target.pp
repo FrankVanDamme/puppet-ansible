8e32bdf6b16e59d15bd8f6e89da43703f43ed67b
Frank Van Damme <frank.van.damme@vub.be>
2023-06-09 14:55:12 +0200

    target: rewrote case statement for python_package and defaulted RedHat to "python3"

--- manifests/target.pp
+++ manifests/target.pp
@@ -16,9 +16,9 @@ class ansible::target {
   include ansible::user
 
   sshkeys::set_authorized_keys { "ansible":
-    keyname  => "ansible_$environment",
-    user     => "ansible",
-    ensure   => present,
+    ensure  => present,
+    keyname => "ansible_${environment}",
+    user    => "ansible",
   }
 
   # Add this target to the "puppetized" inventory group on controllers.
@@ -26,18 +26,30 @@ class ansible::target {
 
   # Make sure python is installed, or it's going to be a really short trip.
 
-  # n/a duidt op RedHat/CentOS 8: de distro codename is uit lsb info verdwenen
-  if ( $facts[os][name] == "Ubuntu" ){
-    $python_package = "python3"
-  } else {
-    $python_package = $facts[os][distro][codename] ? {
-      undef         => "python3",
+  $python_package = $::facts[os][name] ? {
+    "Ubuntu" => "python3",
+
+    "Debian" => $facts[os][release][major] ? {
+      # Alle Debian =< 10
+      /(9|10)/ => "python",
+      # Debian vanaf versie 11
+      default  => "python3",
+    },
+
+    default  => $facts[os][family] ? {
       # Red Hat
-      /(Core|n\/a)/ => "python3",
-      "Ootpa"       => "python36",
-      # Debian
-      "bullseye"    => "python3",
-      default       => "python",
+      "RedHat" => $facts[os][distro][codename] ? {
+        # RHEL8
+        /(Ootpa)/     => "python36",
+        # Core=CentOS7
+        # n/a duidt op RedHat/CentOS 8: de distro codename is uit lsb info verdwenen
+        /(Core|n\/a)/ => "python3",
+        undef         => "python3",
+        default       => "python3",
+      },
+
+      # catchall
+      default  => "python",
     }
   }
 
@@ -46,10 +58,10 @@ class ansible::target {
   # If I'm using SELinux on Redhat, make sure the python binding is here.
   if ( ( $facts[os][selinux][enabled] ) and ( $facts[os][family] == 'RedHat' ) ) {
     # n/a duidt op RedHat/CentOS 8: de distro codename is uit lsb info verdwenen
-    $python_selinux_package = $lsbdistcodename ? {
+    $python_selinux_package = $facts[os][distro][codename] ? {
         /(Core|Ootpa|n\/a)/ => "python3-libselinux",
         default             => "libselinux-python",
     }
-    ensure_packages("$python_selinux_package", { 'ensure' => 'present' })
+    ensure_packages($python_selinux_package, { 'ensure' => 'present' })
   }
 }
